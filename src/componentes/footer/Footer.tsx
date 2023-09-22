8b22e56c8de2c4f6708ea7c9896e0c62f8eb84cd
stellinha16 <stellagiovana846@gmail.com>
2023-09-22 07:38:56 -0400

    aa

--- src/componentes/footer/Footer.tsx
+++ src/componentes/footer/Footer.tsx
@@ -10,7 +10,7 @@ export function Footer(){
       </div>
      <ul className='Lista'>
     <li>
-        <h3>Historico</h3>
+        <h3>Historic</h3>
     </li>
     <li>
         <Link texto='Ifms'/>
